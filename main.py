db05ae3e5cecb093d5eabe2223d920a634158b15
cressidal <79559961+cressidal@users.noreply.github.com>
2021-07-26 23:57:52 +1000

    added recurring email feature
    Fixed janky code

--- main.py
+++ main.py
@@ -2,22 +2,33 @@ import smtplib
 import datetime as dt
 import time
 
-def send_email(SUBJECT,TEXT):
+# send the email
+def send_email():
   FROM = "email@gmail.com"
   TO = "email@gmail.com"
+  SUBJECT = "lorem ipsum"
+  TEXT = """lorem ipsum"""
   message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
-
+  
   SERVER = smtplib.SMTP("smtp.gmail.com", 587)
   SERVER.starttls()
   SERVER.login(FROM, "password")
   SERVER.sendmail(FROM, TO, message)
   SERVER.quit()
 
-SUBJECT = "lorem ipsum"
-
-TEXT = """lorem ipsum"""
-
-def send_email_time():
-  send_time = dt.datetime(2021,7,26,23,35,0) #time in AEST
-  time.sleep(send_time.timestamp()-time.time())
+# send email at time
+def send_at(send_time):  
+  time.sleep(send_time.timestamp() - time.time())
   send_email(SUBJECT,TEXT)
+  print("email sent!")
+
+# Send a reccuring email
+init_time = dt.datetime(2021,7,26,23,35,0) # time first email sent
+end_time = dt.datetime (2021,7,27,23,36,0) # time period ends
+interval = dt.timedelta(minutes=2*60) # interval between each email
+
+# loop
+send_time = init_time
+while (time.time() - end_time < 0):
+  send_at(send_time)
+  send_time = init_time + interval
